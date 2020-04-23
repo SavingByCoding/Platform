49a838fed75e703ddd3536e7383b3cb25a71f029
Jeevanantham Sundaram Murugan <jeevanantham.Sundaram.murugan@gmail.com>
2020-04-23 19:21:37 -0400

    New Login.html New Login.css

--- SignUpPage/SignUpFirebase.js
+++ SignUpPage/SignUpFirebase.js
@@ -13,6 +13,26 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
+GoogleSignUp=()=> {
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        console.log("works");
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
+
 RegularSignUp=()=>{
     let email= document.getElementById('username').value;
     console.log(email);
