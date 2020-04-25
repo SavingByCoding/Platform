cb35b20a566bbe702e35b2af2afd98b6c90039eb
Jeevanantham Sundaram Murugan <jeevanantham.Sundaram.murugan@gmail.com>
2020-04-24 22:13:38 -0400

    Homework Page Sandbox Page

---
+++ HomeworkPage/HomeworkEditor.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    $("#CompileButton").click(function () {
+        let Code= editor.getValue();
+        Code= encodeURI(Code);
+        Code= Code.replace(/#/g,"~"); //To make sure Comments Work
+        let Output="";
+        let Url= "https://cors-anywhere.herokuapp.com/http://18.218.244.255:8080/"+ Code;
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', Url, true);
+        xhr.send();
+        xhr.onreadystatechange = processRequest;
+
+        function processRequest(e) {
+            Output= xhr.responseText;
+            insertText(Output);
+        }
+
+        function insertText(text) {
+            document.getElementById("outputScreen").value= text;
+        }
+
+
+    });
+
+});
