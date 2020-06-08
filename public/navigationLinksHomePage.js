0048b9a76add0fbe6bcdfb0368a94899b7dc659a
neelpatil09 <62684641+neelpatil09@users.noreply.github.com>
2020-06-08 18:12:21 -0400

    neel

--- public/navigationLinksHomePage.js
+++ public/navigationLinksHomePage.js
@@ -1,20 +1,20 @@
 function registerNow(){
-    window.open("../LoginPage/Login.html")
+    window.open(" LoginPage/Login.html")
     window.close(this)
 }
 var mainMod = angular.module("myApp", []);
 mainMod.controller("myCont1", function ($scope) {
 
-    $scope.logo = "../LogoWithWordsWhite.png";
+    $scope.logo = " LogoWithWordsWhite.png";
 
 
     $scope.menuName1 = "Home";
-    $scope.menuName1Link = "../index.html";
+    $scope.menuName1Link = " index.html";
     let m1 = document.getElementById("m1")
     m1.href = $scope.menuName1Link;
 
     $scope.menuName2 = "About Us";
-    $scope.menuName2Link = "../AboutPage/AboutPage.html";
+    $scope.menuName2Link = " AboutPage/AboutPage.html";
     let m2 = document.getElementById("m2")
     m2.href = $scope.menuName2Link;
 
@@ -22,38 +22,38 @@ mainMod.controller("myCont1", function ($scope) {
 
 
     $scope.menuName4 = "Contact Us";
-    $scope.menuName4Link = "../ContactPage/contact.html";
+    $scope.menuName4Link = " ContactPage/contact.html";
     let m4 = document.getElementById("m4")
     m4.href = $scope.menuName4Link;
 
     $scope.menuName5 = "HTML Course Overview";
-    $scope.menuName5Link = "../HTMLContentPage/HTMLContentPage.html";
+    $scope.menuName5Link = " HTMLContentPage/HTMLContentPage.html";
     let m5 = document.getElementById("m5")
     m5.href = $scope.menuName5Link;
 
     $scope.menuName6 = "Python Course Overview";
-    $scope.menuName6Link = "../PythonContentPage/PythonContentPage.html";
+    $scope.menuName6Link = " PythonContentPage/PythonContentPage.html";
     let m6 = document.getElementById("m6")
     m6.href = $scope.menuName6Link;
 
 
-    // $scope.registerNowLink= "../RegistrationForm/registration.html";
+    // $scope.registerNowLink= " RegistrationForm/registration.html";
     // $("#RegisterNow").attr("href",$scope.registerNowLink); //Added a registernow link so you could just put the link here //UNCOMMENT LATER
 
     $scope.dropdownName = "Profile";
 
     $scope.sand1 = "HTML Sandbox"
-    $scope.sandboxHTMLLink = "../SandboxPageHtml/Sandbox.html"
+    $scope.sandboxHTMLLink = " SandboxPageHtml/Sandbox.html"
     let s1 = document.getElementById("s1")
     s1.href = $scope.sandboxHTMLLink
 
     $scope.sand2 = "Python Sandbox"
-    $scope.sandboxPythonLink = "../SandboxPagePython/Sandbox.html"
+    $scope.sandboxPythonLink = " SandboxPagePython/Sandbox.html"
     let s2 = document.getElementById("s2")
     s2.href = $scope.sandboxPythonLink;
 
     $scope.dropdown1 = "View Profile"
-    $scope.dropdown1Link = "../ProfilePage/profile.html";
+    $scope.dropdown1Link = " ProfilePage/profile.html";
     let d1 = document.getElementById("d1")
     d1.href = $scope.dropdown1Link;
 
@@ -63,22 +63,22 @@ mainMod.controller("myCont1", function ($scope) {
     // d2.href = $scope.dropdown2Link;
 
     // $scope.dropdown3 = "Sign-up"
-    // $scope.dropdown3Link = "../SignUpPage/SignUp.html";
+    // $scope.dropdown3Link = " SignUpPage/SignUp.html";
     // let d3 = document.getElementById("d3")
     // d3.href = $scope.dropdown3Link;
 
     $scope.dropdown4 = "Logout"
-    $scope.dropdown4Link = "../index.html"; //not done
+    $scope.dropdown4Link = " index.html"; //not done
     let d4 = document.getElementById("d4")
     d4.href = $scope.dropdown4Link;
 
     $scope.dropdown5 = "SignUp/Login"
-    $scope.dropdown5Link = "../LoginPage/Login.html";
+    $scope.dropdown5Link = " LoginPage/Login.html";
     let d5 = document.getElementById("d5")
     d5.href = $scope.dropdown5Link
 
     $scope.dropdown6 = "Admin Portal"
-    $scope.dropdown6Link = "../AdminPage/admin.html"
+    $scope.dropdown6Link = " AdminPage/admin.html"
     let d6 = document.getElementById("d6")
     d6.href = $scope.dropdown6Link
 
@@ -156,44 +156,44 @@ mainMod.controller("myCont1", function ($scope) {
 
 mainMod.controller("MyCont2", function ($scope) {
     $scope.footerMenuName1 = "Home";
-    $scope.footerMenuName1Link = "../index.html";
+    $scope.footerMenuName1Link = " index.html";
     let f1 = document.getElementById("f1")
     f1.href = $scope.footerMenuName1Link;
 
     $scope.footerMenuName2 = "About Us";
-    $scope.footerMenuName2Link = "../AboutPage/AboutPage.html";
+    $scope.footerMenuName2Link = " AboutPage/AboutPage.html";
     let f2 = document.getElementById("f2")
     f2.href = $scope.footerMenuName2Link;
 
     // $scope.footerMenuName3 = "Register Now";
-    // $scope.footerMenuName3Link = "../RegistrationForm/registration.html";
+    // $scope.footerMenuName3Link = "RegistrationForm/registration.html";
     // let f3 = document.getElementById("f3")
     // f3.href = $scope.footerMenuName3Link;
 
     $scope.footerMenuName4 = "Contact Us";
-    $scope.footerMenuName4Link = "../ContactPage/contact.html";
+    $scope.footerMenuName4Link = " ContactPage/contact.html";
     let f4 = document.getElementById("f4")
     f4.href = $scope.footerMenuName4Link;
 
     $scope.footerMenuName5 = "HTML Course Overview";
-    $scope.footerMenuName5Link = "../HTMLContentPage/HTMLContentPage.html";
+    $scope.footerMenuName5Link = " HTMLContentPage/HTMLContentPage.html";
     let f5 = document.getElementById("f5")
     f5.href = $scope.footerMenuName5Link;
 
     $scope.footerMenuName6 = "Python Course Overview";
-    $scope.footerMenuName6Link = "../PythonContentPage/PythonContentPage.html";
+    $scope.footerMenuName6Link = " PythonContentPage/PythonContentPage.html";
     let f6 = document.getElementById("f6")
     f6.href = $scope.footerMenuName6Link;
 
     $scope.footerMenuName7 = "Volunteer";
-    $scope.footerMenuName7Link = "../ContactPage/contact.html";
+    $scope.footerMenuName7Link = " ContactPage/contact.html";
     let f7 = document.getElementById("f7")
     f7.href = $scope.footerMenuName7Link;
 
 });
 
 function goBack(){
-    window.open("../ProfilePage/profile.html")
+    window.open(" ProfilePage/profile.html")
     window.close(this)
 }
 
