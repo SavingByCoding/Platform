7c504e7b3ecc8bd1513c4bc46433cfb4592c3a06
Jeevanantham Sundaram Murugan <jeevanantham.Sundaram.murugan@gmail.com>
2020-08-08 15:33:41 -0400

    Revamped the Projects in the Sandbox Created Display Projects Created Load Projects Created Save Projects Removed old project system

--- public/LoginPage/LoginFirebase.js
+++ public/LoginPage/LoginFirebase.js
@@ -7,7 +7,6 @@ let ProfilePicURL;
 GoogleSignIn=()=> {
     firebase.auth().signInWithPopup(provider).then(function (result) {
         var token = result.credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
         id=user.uid
         email= user.email;
@@ -15,8 +14,6 @@ GoogleSignIn=()=> {
         ProfilePicURL= user.photoURL;
         userInit(id,function () {
             setTimeout(function(){window.location='../ProfilePage/profile.html'}, 2000)
-            // window.open ('../ProfilePage/profile.html','_self',false);
-            // console.log("here")
         });
     }).catch(function (error) {
         // Handle Errors here.
@@ -35,82 +32,71 @@ function userInit (id,callback1){
     db.collection('users').doc(id).get() //Checks if the Document Exists
         .then((docSnapshot) => {
             if (!docSnapshot.exists) {
-                CreateAccountInDB(id,AllocateSpaceInDB); //Creates an account for the user in the DB
-                //AllocateSpaceInDB(id); //Allocates Project Space for the User
+                CreateAccountInDB(id); //Creates an account for the user in the DB
             }
         });
     callback1();
 }
 
-// const generateUUID = () => { // V4
-//     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-//         var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8)
-//         return v.toString(16)
-//     })
-// }
-
-CreateAccountInDB= (id,callback2) => {
+CreateAccountInDB= (id) => {
     let data = {
         userId: id,
         email: email,
-        name: FullName, //Add name from input
+        name: FullName,
         userType: "1",
-        profilepictureURL: ProfilePicURL, //Change to Date of Birth from input
+        profilepictureURL: ProfilePicURL, 
         dateJoined: getCurrentDate(),
-        isRegistered: false,
-        isSpaceAllocated: true,
         isAccountCreated: false
     };
     db.collection('users').doc(id).set(data);
-    callback2(id);
 };
 
 
-AllocateSpaceInDB= (id) => {
-    let project1UUID= generateUUID();
-    let project2UUID= generateUUID();
-    let project3UUID= generateUUID();
-    let project4UUID=generateUUID();
-    let project5UUID=generateUUID();
-    let project6UUID=generateUUID();
-    let project7UUID=generateUUID();
-    let project8UUID=generateUUID();
-    let project9UUID=generateUUID();
-    let project10UUID=generateUUID();
-
-    let projectsuuid= [];
-    projectsuuid.push(project1UUID,project2UUID,project3UUID,project4UUID,project5UUID,project6UUID,project7UUID,project8UUID,project9UUID,project10UUID);
-
-    let projects = {
-        Project1: project1UUID,
-        Project2: project2UUID,
-        Project3: project3UUID,
-        Project4: project4UUID,
-        Project5: project5UUID,
-        Project6: project6UUID,
-        Project7: project7UUID,
-        Project8: project8UUID,
-        Project9: project9UUID,
-        Project10: project10UUID
-    };
-    console.log(projectsuuid)
-    db.collection('user-projects').doc(id).set(projects);
-    FormatProjects(projectsuuid);
-
-};
-
-function FormatProjects (array){
-    let ProjectFormat= {
-        code: "",
-        language:"EMPTY",
-        name: "Empty Project"
-
-    };
-    array.forEach(function (uuid,index) {
-        db.collection('projects').doc(uuid).set(ProjectFormat);
-    });
-    //window.open ('../ProfilePage/profile.html','_self',false);
-}
+// AllocateSpaceInDB= (id) => {
+//     let project1UUID= generateUUID();
+//     let project2UUID= generateUUID();
+//     let project3UUID= generateUUID();
+//     let project4UUID=generateUUID();
+//     let project5UUID=generateUUID();
+//     let project6UUID=generateUUID();
+//     let project7UUID=generateUUID();
+//     let project8UUID=generateUUID();
+//     let project9UUID=generateUUID();
+//     let project10UUID=generateUUID();
+//
+//     let projectsuuid= [];
+//     projectsuuid.push(project1UUID,project2UUID,project3UUID,project4UUID,project5UUID,project6UUID,project7UUID,project8UUID,project9UUID,project10UUID);
+//
+//     let projects = {
+//         Project1: project1UUID,
+//         Project2: project2UUID,
+//         Project3: project3UUID,
+//         Project4: project4UUID,
+//         Project5: project5UUID,
+//         Project6: project6UUID,
+//         Project7: project7UUID,
+//         Project8: project8UUID,
+//         Project9: project9UUID,
+//         Project10: project10UUID
+//     };
+//     console.log(projectsuuid)
+//     db.collection('user-projects').doc(id).set(projects);
+//     FormatProjects(projectsuuid);
+//
+// };
+//
+// function FormatProjects (array){
+//     let ProjectFormat= {
+//         code: "",
+//         language:"EMPTY",
+//         name: "Empty Project"
+//
+//     };
+//     array.forEach(function (uuid,index) {
+//         db.collection('projects').doc(uuid).set(ProjectFormat);
+//     });
+//     //window.open ('../ProfilePage/profile.html','_self',false);
+// }
 
 
 //
