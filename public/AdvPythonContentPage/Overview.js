796f0d17fc7bfd48f1d8b51af8f7a0e5cef123bc
KrishN33 <krishnaik333@gmail.com>
2020-08-23 11:11:26 -0400

    Advacned Python Overview
    Created Advanced Python Content Overview Page

---
+++ public/AdvPythonContentPage/Overview.js
@@ -0,0 +1,184 @@
+function Unit1() {
+
+
+    if(document.getElementById("Unit1Chapters").style.display == "block")
+    {
+        document.getElementById("Unit1Chapters").style.display = "none";
+
+    }
+    else{
+        document.getElementById("Unit1Chapters").style.display = "block";
+        document.getElementById("Unit1Chapters").style.animation = "fadeInAnimation1 1s 1";
+    }
+
+}
+function Unit2() {
+    if(document.getElementById("Unit2Chapters").style.display == "block")
+    {document.getElementById("Unit2Chapters").style.display = "none";}
+    else{
+        document.getElementById("Unit2Chapters").style.display = "block";
+        document.getElementById("Unit2Chapters").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+
+function Unit3() {
+    if(document.getElementById("Unit3Chapters").style.display == "block")
+    {document.getElementById("Unit3Chapters").style.display = "none";}
+    else{
+        document.getElementById("Unit3Chapters").style.display = "block";
+        document.getElementById("Unit3Chapters").style.animation = "fadeInAnimation1 1s 1";
+
+    }
+
+}
+function Unit4() {
+    if(document.getElementById("Unit4Chapters").style.display == "block")
+    {document.getElementById("Unit4Chapters").style.display = "none";
+    }
+    else{
+        document.getElementById("Unit4Chapters").style.display = "block";
+        document.getElementById("Unit4Chapters").style.animation = "fadeInAnimation1 1s 1";
+    }
+
+}
+
+function Chapter1() {
+    if(document.getElementById("Chapter1").style.display == "block")
+    {document.getElementById("Chapter1").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter1").style.display = "block";
+        document.getElementById("Chapter1").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter2(){
+    if(document.getElementById("Chapter2").style.display == "block")
+    {document.getElementById("Chapter2").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter2").style.display = "block";
+        document.getElementById("Chapter2").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+
+function Chapter3(){
+    if(document.getElementById("Chapter3").style.display == "block")
+    {document.getElementById("Chapter3").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter3").style.display = "block";
+        document.getElementById("Chapter3").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter4(){
+    if(document.getElementById("Chapter4").style.display == "block")
+    {document.getElementById("Chapter4").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter4").style.display = "block";
+        document.getElementById("Chapter4").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter5(){
+    if(document.getElementById("Chapter5").style.display == "block")
+    {document.getElementById("Chapter5").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter5").style.display = "block";
+        document.getElementById("Chapter5").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter6(){
+    if(document.getElementById("Chapter6").style.display == "block")
+    {document.getElementById("Chapter6").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter6").style.display = "block";
+        document.getElementById("Chapter6").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter7(){
+    if(document.getElementById("Chapter7").style.display == "block")
+    {document.getElementById("Chapter7").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter7").style.display = "block";
+        document.getElementById("Chapter7").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter8(){
+    if(document.getElementById("Chapter8").style.display == "block")
+    {document.getElementById("Chapter8").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter8").style.display = "block";
+        document.getElementById("Chapter8").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter9(){
+    if(document.getElementById("Chapter9").style.display == "block")
+    {document.getElementById("Chapter9").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter9").style.display = "block";
+        document.getElementById("Chapter9").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter10(){
+    if(document.getElementById("Chapter10").style.display == "block")
+    {document.getElementById("Chapter10").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter10").style.display = "block";
+        document.getElementById("Chapter10").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter11(){
+    if(document.getElementById("Chapter11").style.display == "block")
+    {document.getElementById("Chapter11").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter11").style.display = "block";
+        document.getElementById("Chapter11").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter12(){
+    if(document.getElementById("Chapter12").style.display == "block")
+    {document.getElementById("Chapter12").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter12").style.display = "block";
+        document.getElementById("Chapter12").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+function Chapter13(){
+    if(document.getElementById("Chapter13").style.display == "block")
+    {document.getElementById("Chapter13").style.display = "none";
+    }
+    else{
+        document.getElementById("Chapter13").style.display = "block";
+        document.getElementById("Chapter13").style.animation = "fadeInAnimation1 1s 1";
+    }
+}
+
+function closeAll(){
+    document.getElementById("Unit1Chapters").style.display = "none";
+    document.getElementById("Unit2Chapters").style.display = "none";
+    document.getElementById("Unit3Chapters").style.display = "none";
+    document.getElementById("Unit4Chapters").style.display = "none";
+    document.getElementById("Chapter1").style.display = "none";
+    document.getElementById("Chapter2").style.display = "none";
+    document.getElementById("Chapter3").style.display = "none";
+    document.getElementById("Chapter4").style.display = "none";
+    document.getElementById("Chapter5").style.display = "none";
+    document.getElementById("Chapter6").style.display = "none";
+    document.getElementById("Chapter7").style.display = "none";
+    document.getElementById("Chapter8").style.display = "none";
+    document.getElementById("Chapter9").style.display = "none";
+    document.getElementById("Chapter10").style.display = "none";
+    document.getElementById("Chapter11").style.display = "none";
+    document.getElementById("Chapter12").style.display = "none";
+    document.getElementById("Chapter13").style.display = "none";
+
+
+}
